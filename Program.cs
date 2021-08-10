21f65c9241112aef77df2f977f1710618d6705c4
MetoA <metoandov@gmail.com>
2021-08-10 17:20:24 +0200

    random comment

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace VP_Sudoku
 {
     static class Program
     {
+	//Random comment for test
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
